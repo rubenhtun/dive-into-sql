22ad559a1c4f904daad6041fd1b37ac064a265d4
Ruben Htun <rubenhtun2003@gmail.com>
2025-06-29 21:26:52 +0700

    removed all files

--- chapter-4/04-select-employees-not-manager.sql
+++
@@ -1,4 +0,0 @@
--- Select all employees who are NOT Managers
-SELECT name, position, salary
-FROM employees
-WHERE NOT position = 'Manager';
--- chapter-4/05-select-high-salary-or-young-it.sql
+++
@@ -1,6 +0,0 @@
--- Select employees with salary greater than 5000
--- OR who work in the IT department and are under 30 years old
-SELECT name, salary, department
-FROM employees
-WHERE salary > 5000
-   OR (department = 'IT' AND age < 30);
