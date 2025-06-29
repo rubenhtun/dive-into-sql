93fc4e7d40c075472e3089b06fed489e34e91cbb
Ruben Htun <rubenhtun2003@gmail.com>
2025-06-29 21:35:34 +0700

    last update

--- chapter-4/03-select-employees-5000-male-or-female.sql
+++
@@ -1,5 +0,0 @@
--- Select employees with salary 5000 and gender either male ('M') or female ('F')
-SELECT name, salary, gender
-FROM employees
-WHERE salary = 5000
-  AND (gender = 'M' OR gender = 'F');
