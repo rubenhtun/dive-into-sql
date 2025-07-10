afd42be3aa50ebfaa78a9bdbc92bb811c248e6f9
Ruben Htun <rubenhtun2003@gmail.com>
2025-07-11 01:48:25 +0700

    test: Select all columns and specific columns from customers

---
+++ chapter-12/06-select-customers-columns.sql
@@ -0,0 +1,5 @@
+-- Select all columns from customers table
+SELECT * FROM customers;
+
+-- Select only customer_name and phone columns from customers table
+SELECT customer_name, phone FROM customers;
